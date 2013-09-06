32f255786f83a71c4faba235825d8e6da88572c3
Muhammad Waris Ali <muhammadwarisali@Muhammads-Mac-mini.local>
2013-09-06 15:32:04 +0500

    Initial Commit

---
+++ MyStore/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MyStore
+//
+//  Created by Muhammad Waris Ali on 06/09/2013.
+//  Copyright (c) 2013 Muhammad Waris Ali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
